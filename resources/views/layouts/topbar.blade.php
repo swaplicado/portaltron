10d266acf92470c80e88fb769c9bb7775152c6f8
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2023-10-17 10:34:17 -0600

    fixes

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -85,11 +85,7 @@
           </li>
 
           <!-- boton side derecha -->
-          <li class="nav-item nav-settings d-none d-lg-flex">
-            <a class="nav-link" href="#">
-              <i class="icon-ellipsis"></i>
-            </a>
-          </li>
+          
         </ul>
 
         <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
