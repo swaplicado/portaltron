d059c1ddef68bbcd117f5999690d0b695159b535
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2025-06-13 12:31:58 -0600

    Cambios en Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,10 @@
 <footer class="footer">
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <strong>Copyright &copy; <?php echo date('Y'); ?>  Software Aplicado.</strong> Todos los derechos reservados.
+    <div class="row justify-content-center">
+        <div class="col-12 d-flex justify-content-center align-items-center" style="font-size: 14px; gap: 10px; text-align: center; flex-wrap: wrap;">
+            <span>© {{ \Carbon\Carbon::now()->year }} Software Aplicado SA de CV. Todos los derechos reservados.</span>
+            <a href="https://www.swaplicado.com.mx" target="_blank" style="text-decoration: none; color: inherit;">
+                www.swaplicado.com.mx
+            </a>
+        </div>
     </div>
 </footer>
